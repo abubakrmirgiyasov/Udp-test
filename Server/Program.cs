1aa8ed16270d5a11cbe6da82a09e78330539930e
Abubakr <abubakrmirgiyasov@gmail.com>
2023-10-05 18:00:28 +0700

    Added server proj

---
+++ Server/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+Console.Write("Введите ip: ");
+var ip = IPAddress.Parse(Console.ReadLine()!);
+
+var endpoint = new IPEndPoint(ip, 5555);
+var receiverClient = new UdpClient(endpoint);
+
+var data = receiverClient.Receive(ref endpoint);
+
+int sentPackets = 0;
+int receivedPackets = 256;
+
+for (int i = 0; i < data.Length; i++)
+{
+    sentPackets++;
+}
+
+Console.WriteLine($"Потеряно пакетов: {receivedPackets}");
